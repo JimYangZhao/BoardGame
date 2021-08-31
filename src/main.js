21516903d71dc25d616cdc6b40bd241b3f00e6e4
Yang Zhao <zhaoyang@jixiang.cn>
2021-08-31 09:00:04 +0800

    update for font

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import App from './App.vue';
 import 'lib-flexible';
 import router from './router/index.js';
 import './css/home.css';
-
+import './font/font.css'
 
 Vue.config.productionTip = false
-
 
 new Vue({
   render: h => h(App),
